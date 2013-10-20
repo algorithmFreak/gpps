6723e331223f31119d13125dd254c1f6f6d48f5e
pratik palashikar <pratikpalashikar@gmail.com>
2013-10-20 13:42:24 +0530

    initial project version

---
+++ src/com/whitePaper/publicpositioningsystem/Activity_login.java
@@ -0,0 +1,65 @@
+package com.whitePaper.publicpositioningsystem;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Activity_login extends Activity {
+
+	private Button button;
+	private Button createNew_button; 
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.login);
+		addListenerOnButton();
+		
+	}
+
+	public void addListenerOnButton() {
+
+		final Context context = this;
+
+		button = (Button) findViewById(R.id.login);
+		createNew_button=(Button)findViewById(R.id.createNew);
+
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View arg0) {
+				final TextView username =(TextView)findViewById(R.id.username);
+        		final TextView password =(TextView)findViewById(R.id.password);
+        		String uname = username.getText().toString();
+        		String pass =  password.getText().toString();
+
+        		if(uname.equals("pratik") && pass.equals("pratik")){
+				Intent intent = new Intent(context, MapX.class);
+				startActivity(intent);
+        		}
+        		else{
+        			Toast.makeText(Activity_login.this,"Invalid UserName or Password", Toast.LENGTH_LONG).show();
+        		}
+
+			}
+			
+		});
+		
+		createNew_button.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				Intent intent = new Intent(context, SignUp.class);
+				startActivity(intent); 
+			}
+		});
+
+	}
+
+}
---
+++ src/com/whitePaper/publicpositioningsystem/SignUp.java
@@ -0,0 +1,15 @@
+package com.whitePaper.publicpositioningsystem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SignUp extends Activity{
+	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.signup);
+	}
+
+
+}
